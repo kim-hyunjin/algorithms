878ba6e33c92fb1a5731057f5aeabcda83bedf41
Kim <hyunjin1612@gmail.com>
2021-03-15 22:22:37 +0900

    다리 위 트럭 알고리즘 속도 향상시키기

--- src/js/truck_and_bridge/solution.js
+++ src/js/truck_and_bridge/solution.js
@@ -1,8 +1,9 @@
 function solution(bridge_length, weight, truck_weights) {
-  var answer = 0; // 총 걸린 시간
+  var answer = 0;
   const trucks = truck_weights.slice();
   const passed = [];
   const bridge = [];
+  let bridgeWeight = 0;
   for (let i = 0; i < bridge_length; i++) {
     bridge[i] = 0;
   }
@@ -11,16 +12,14 @@ function solution(bridge_length, weight, truck_weights) {
     let passedTruck = bridge.shift();
     bridge[bridge_length - 1] = 0;
     if (passedTruck != 0) {
+      bridgeWeight -= passedTruck;
       passed.push(passedTruck);
     }
 
-    let truck = trucks.shift();
-    if (truck) {
-      let bridgeWeight = bridge.reduce((acc, cur) => acc + cur, 0);
-      if (bridgeWeight + truck <= weight) {
-        bridge[bridge_length - 1] = truck;
-      } else {
-        trucks.unshift(truck);
+    if (trucks[0]) {
+      if (bridgeWeight + trucks[0] <= weight) {
+        bridgeWeight += trucks[0];
+        bridge[bridge_length - 1] = trucks.shift();
       }
     }
 
