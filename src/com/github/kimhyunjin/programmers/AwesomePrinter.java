8daf8a2e5e8ff9ff26e63907b1262b2bc822bf10
Kim <hyunjin1612@gmail.com>
2021-03-27 20:36:06 +0900

    프로그래머스 프린터문제 다른 사람 풀이

--- src/com/github/kimhyunjin/programmers/AwesomePrinter.java
+++ src/com/github/kimhyunjin/programmers/AwesomePrinter.java
@@ -9,7 +9,7 @@ public class AwesomePrinter {
     public static void main(String[] args) {
         int[] priorities = {2, 1, 3, 2};
         int location = 2;
-        System.out.println(solution(priorities, location));
+        System.out.println(solution2(priorities, location));
     }
 
     private static int solution(int[] priorities, int location) {
@@ -32,4 +32,40 @@ public class AwesomePrinter {
             }
         }
     }
+
+    private static int solution2(int[] priorities, int location) {
+        int answer = 0;
+        int l = location;
+        Queue<Integer> printQueue = new LinkedList<>();
+        for (int i : priorities) {
+            printQueue.add(i);
+        }
+
+        Arrays.sort(priorities);
+        int maxLocation = priorities.length - 1;
+
+        while(!printQueue.isEmpty()) {
+            // 문서를 앞에서부터 꺼내 우선순위 비교
+            Integer item = printQueue.poll();
+            if (item == priorities[maxLocation - answer]) {
+                answer++;
+                l--;
+                if (l < 0) {
+                    // 추적 중인 문서의 우선순위가 가장 높으면서 현재 위치가 0보다 작아졌다는 것은 큐에서 빠져나왔다는것
+                    break;
+                }
+            } else {
+                // 문서의 우선순위가 가장 높지 않으면 다시 큐에 집어넣는다.
+                printQueue.add(item);
+                l--;
+                if (l < 0) {
+                    // 추적 중인 문서가 남아 있는 문서들보다 우선순위가 높지 않으면서 현재 위치가 0보다 작아졌다는 것은 큐의 맨 뒤로 이동했다는것
+                    l = printQueue.size() - 1;
+                }
+            }
+        }
+
+        return answer;
+    }
+
 }
