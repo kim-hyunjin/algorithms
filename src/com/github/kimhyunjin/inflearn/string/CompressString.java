81e39b1ac7b1069b84148b433c2fecf559a0f5e0
kim-hyunjin <hyunjin1612@gmail.com>
2021-10-05 22:57:59 +0900

    문자열 압축 문제풀이 추가

--- src/com/github/kimhyunjin/inflearn/string/CompressString.java
+++ src/com/github/kimhyunjin/inflearn/string/CompressString.java
@@ -22,11 +22,26 @@ public class CompressString {
         }
         return sb.toString();
     }
+    public String solution2(String input) {
+        String answer = "";
+        input += " "; // index out of bound 방지
+        int repeatCnt = 1;
+        for (int i = 0; i < input.length() - 1; i++) { // 뒤에 임의로 빈문자 추가했으므로 length - 1까지만 순회
+            if (input.charAt(i) == input.charAt(i + 1)) {
+                repeatCnt++;
+            } else {
+                answer += input.charAt(i);
+                if (repeatCnt > 1) answer += String.valueOf(repeatCnt);
+                repeatCnt = 1;
+            }
+        }
+        return answer;
+    }
     public static void main(String[] args){
         CompressString compressString = new CompressString();
         Scanner in=new Scanner(System.in);
         String input = in.nextLine();
-        System.out.println(compressString.solution(input));
+        System.out.println(compressString.solution2(input));
         return ;
     }
 }
