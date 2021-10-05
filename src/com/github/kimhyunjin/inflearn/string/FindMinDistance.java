f369a05c5b3f75dc09277e18a0d639a92ab73cce
kim-hyunjin <hyunjin1612@gmail.com>
2021-10-05 22:47:07 +0900

    가장 짧은 문자거리 문제풀이 추가

--- src/com/github/kimhyunjin/inflearn/string/FindMinDistance.java
+++ src/com/github/kimhyunjin/inflearn/string/FindMinDistance.java
@@ -27,12 +27,35 @@ public class FindMinDistance {
         }
         return distance;
     }
+
+    public int[] solution2(String str, char c) {
+        int[] answer = new int[str.length()];
+        int p = 101; // 문자열의 길이는 100을 넘지 않으므로
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == c) {
+                p = 0;
+            } else {
+                p++;
+            }
+            answer[i] = p;
+        }
+        p = 101;
+        for (int i = str.length() - 1; i >=0; i--) {
+            if (str.charAt(i) == c) {
+                p = 0;
+            } else {
+                p++;
+                answer[i] = Math.min(answer[i], p);
+            }
+        }
+        return answer;
+    }
     public static void main(String[] args){
         FindMinDistance findMinDistance = new FindMinDistance();
         Scanner in=new Scanner(System.in);
         String str = in.next();
         String character = in.next();
-        for (int i : findMinDistance.solution(str, character.charAt(0))) {
+        for (int i : findMinDistance.solution2(str, character.charAt(0))) {
             System.out.print(i + " ");
         }
         return ;
