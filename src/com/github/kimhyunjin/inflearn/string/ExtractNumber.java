5a3529462bce5ee5822860f0bea904d3b11bb460
kim-hyunjin <hyunjin1612@gmail.com>
2021-10-05 22:29:05 +0900

    숫자만 추출 문제풀이 추가

--- src/com/github/kimhyunjin/inflearn/string/ExtractNumber.java
+++ src/com/github/kimhyunjin/inflearn/string/ExtractNumber.java
@@ -3,14 +3,23 @@ package com.github.kimhyunjin.inflearn.string;
 import java.util.Scanner;
 
 public class ExtractNumber {
-    public String solution(String input) {
+    public int solution(String input) {
         String answer = "";
         for (char c : input.toCharArray()) {
             if (Character.isDigit(c)) {
                 answer += c;
             }
         }
-        return String.valueOf(Integer.parseInt(answer));
+        return Integer.parseInt(answer);
+    }
+    public int solution2(String input) {
+        int answer = 0;
+        for (char c : input.toCharArray()) {
+            if (c >= 48 && c <= 57) { // 0 ~ 9 아스키 번호
+                answer = answer * 10 + (c - 48);
+            }
+        }
+        return answer;
     }
     public static void main(String[] args){
         ExtractNumber extractNumber = new ExtractNumber();
