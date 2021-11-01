3e4d96c7e91f9b95c5f0c63f8dc6947f3cc381aa
kim-hyunjin <hyunjin1612@gmail.com>
2021-11-01 13:40:27 +0900

    소수 주석 추가

--- src/com/github/kimhyunjin/inflearn/array/PrimeNumber.java
+++ src/com/github/kimhyunjin/inflearn/array/PrimeNumber.java
@@ -12,9 +12,10 @@ public class PrimeNumber {
         int answer = 0;
         int[] numbers = new int[n + 1];
         for (int i = 2; i <= n; i++) {
+            // 플래그 0 = 소수, 1 = 소수 아님
             if (numbers[i] == 0) {
                 answer++;
-                // i의 배수들 체크
+                // i의 배수들은 i를 약수로 가지므로 소수가 아님
                 for (int j = i * 2; j <= n; j += i) {
                     numbers[j] = 1;
                 }
