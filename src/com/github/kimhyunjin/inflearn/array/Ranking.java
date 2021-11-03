f84a24960784a6b7c54266116e1d987ca13eac82
kim-hyunjin <hyunjin1612@gmail.com>
2021-11-03 21:57:40 +0900

    등수 구하기, 격자판 최대합 다른풀이 학습 및 복기

--- src/com/github/kimhyunjin/inflearn/array/Ranking.java
+++ src/com/github/kimhyunjin/inflearn/array/Ranking.java
@@ -24,6 +24,18 @@ public class Ranking {
         return result;
     }
 
+    // 불필요하게 따로 정렬할 필요없이 이중 for 문만으로도 단순하게 해결 가능한 문제였음
+    private int[] solution2(int[] scores) {
+        int[] result = new int[scores.length];
+        for (int i = 0; i < scores.length; i++) {
+            result[i] = 1; // 나보다 큰 점수가 없으면 1등이므로 1로 셋팅
+            for (int score : scores) {
+                if (score > scores[i]) result[i] += 1; // 나보다 큰 점수가 있으면 등수가 1씩 밀려난다. -> 이부분이 이 풀이의 주요한 아이디어
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args){
         Ranking ranking = new Ranking();
         Scanner in = new Scanner(System.in);
@@ -32,7 +44,7 @@ public class Ranking {
         for (int i = 0; i < input1; i++) {
             scores[i] = in.nextInt();
         }
-        int[] ranked = ranking.solution(scores);
+        int[] ranked = ranking.solution2(scores);
         for (int i : ranked) {
             System.out.print(i + " ");
         }
