2da2d686404631ac485358b6c3b3d74b7a8b6b36
kim-hyunjin <hyunjin1612@gmail.com>
2021-11-23 20:10:04 +0900

    K번째 큰수 문제 풀이 추가, 테스트코드 작성

--- java-quiz/src/main/java/com/github/kimhyunjin/inflearn/hash/KthBigNumber.java
+++ java-quiz/src/main/java/com/github/kimhyunjin/inflearn/hash/KthBigNumber.java
@@ -1,8 +1,6 @@
 package com.github.kimhyunjin.inflearn.hash;
 
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class KthBigNumber {
 
@@ -24,6 +22,24 @@ public class KthBigNumber {
         }
     }
 
+    // TreeSet을 사용하면 정렬과 동시에 중복을 제거할 수 있다.
+    public static int solution2(int[] arr, int kth) {
+        TreeSet<Integer> treeSet = new TreeSet<>(Collections.reverseOrder()); // 오름차순으로 정렬하기 위해 생성자에 Comparator를 넘겨줌
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                for (int k = j + 1; k < arr.length; k++) {
+                    treeSet.add(arr[i] + arr[j] + arr[k]);
+                }
+            }
+        }
+        int k = kth;
+        for (int i : treeSet) {
+            k--;
+            if (k == 0) return i; // k번째 값이 정답
+        }
+        return -1;
+    }
+
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         int len = in.nextInt();
@@ -36,3 +52,8 @@ public class KthBigNumber {
         return ;
     }
 }
+
+/**
+ * 처음에 문제를 풀때는 Set으로 중복을 없애고 직접 정렬했지만
+ * 강의를 통해, TreeSet을 사용하면 정렬과 동시에 중복을 없앨 수 있다는 것을 알게되었다.
+ */
