a24f5840d369f7224e2ffc435408f5c01707399e
kim-hyunjin <hyunjin1612@gmail.com>
2021-11-24 19:01:52 +0900

    js 버블정렬, 선택정렬, 삽입정렬

--- js-quiz/sort/bubble-sort.js
+++ js-quiz/sort/bubble-sort.js
@@ -1,5 +1,8 @@
 function bubbleSort(arr) {
+  // 인접한 두 값을 비교해서 대소에 따라 위치를 바꾼다.
+  // 큰 수를 뒤쪽에 먼저 정렬
   for (let i = arr.length - 1; i > 0; i--) {
+    // 정렬된 뒷부분을 제외해야하므로 그 앞까지만 비교
     for (let j = 0; j < i; j++) {
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
@@ -12,11 +15,16 @@ function bubbleSort(arr) {
 }
 
 function test() {
+  // given
   let given = [13, 5, 11, 7, 23, 15];
-  bubbleSort(given);
-  console.log(given);
   let expect = [5, 7, 11, 13, 15, 23];
-  console.log(JSON.stringify(given) == JSON.stringify(expect));
+
+  // when
+  let actual = bubbleSort(given);
+  console.log(actual);
+
+  // then
+  console.log(JSON.stringify(expect) == JSON.stringify(actual));
 }
 
 test();
