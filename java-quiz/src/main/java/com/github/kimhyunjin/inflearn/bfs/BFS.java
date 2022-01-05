207de99570a889dc4bab03bbbf3cd4a86f21abc4
kim-hyunjin <hyunjin1612@gmail.com>
2022-01-05 12:32:11 +0900

    말단노드까지 가장 짧은 경로 문제 풀이

--- java-quiz/src/main/java/com/github/kimhyunjin/inflearn/bfs/BFS.java
+++ java-quiz/src/main/java/com/github/kimhyunjin/inflearn/bfs/BFS.java
@@ -29,6 +29,23 @@ public class BFS {
         }
     }
 
+    public int shortestWayToLeafNode(Node root) {
+        Queue<Node> Q = new LinkedList<>();
+        Q.offer(root);
+        int L = 0;
+        while(!Q.isEmpty()) {
+            int len = Q.size();
+            for (int i = 0; i < len; i++) {
+                Node cur = Q.poll();
+                if (cur.lt == null && cur.rt == null) return L;
+                if (cur.lt != null) Q.offer(cur.lt);
+                if (cur.rt != null) Q.offer(cur.rt);
+            }
+            L++;
+        }
+        return -1;
+    }
+
     static class Node {
         int data;
         BFS.Node lt, rt;
@@ -45,9 +62,10 @@ public class BFS {
         tree.root.rt = new Node(3);
         tree.root.lt.lt = new Node(4);
         tree.root.lt.rt = new Node(5);
-        tree.root.rt.lt = new Node(6);
-        tree.root.rt.rt = new Node(7);
+//        tree.root.rt.lt = new Node(6);
+//        tree.root.rt.rt = new Node(7);
 
         tree.search(tree.root);
+        System.out.println(tree.shortestWayToLeafNode(tree.root));
     }
 }
--- java-quiz/src/main/java/com/github/kimhyunjin/inflearn/dfs/DFS.java
+++ java-quiz/src/main/java/com/github/kimhyunjin/inflearn/dfs/DFS.java
@@ -14,6 +14,16 @@ public class DFS {
         }
     }
 
+    // 말단노드까지의 가장 짧은 경로 구하기 문제는 BFS가 더 적합하다.
+    // DFS는 완전이진트리인 경우만 가능하다
+    public int shortestWayToLeafNode(int L, Node root) {
+        if (root.lt == null && root.rt == null) {
+            return L;
+        } else {
+            return Math.min(shortestWayToLeafNode(L+1, root.lt), shortestWayToLeafNode(L+1, root.rt));
+        }
+    }
+
     static class Node {
         int data;
         Node lt, rt;
@@ -30,10 +40,11 @@ public class DFS {
         tree.root.rt = new Node(3);
         tree.root.lt.lt = new Node(4);
         tree.root.lt.rt = new Node(5);
-        tree.root.rt.lt = new Node(6);
-        tree.root.rt.rt = new Node(7);
+//        tree.root.rt.lt = new Node(6);
+//        tree.root.rt.rt = new Node(7);
 
         tree.search(tree.root);
+        System.out.println(tree.shortestWayToLeafNode(0, tree.root));
     }
 
 
