fc02cfc01f562fd791aca60ad79f30724f1ce509
kim-hyunjin <hyunjin1612@gmail.com>
2022-01-18 18:00:21 +0900

    조합수 구하기 다른 풀이

--- java-quiz/src/main/java/com/github/kimhyunjin/inflearn/dfs/Combination.java
+++ java-quiz/src/main/java/com/github/kimhyunjin/inflearn/dfs/Combination.java
@@ -6,6 +6,10 @@ public class Combination {
     static int n, r;
     static int[][] mem;
 
+    /**
+     * 0C0 부터 nCn 까지의 수를 모두 구해 mem 배열에 저장한다.
+     * 재귀는 0 ~ n까지 수행된다.
+     */
     public void DFS(int L) {
         if(L == n + 1) {
             return;
@@ -19,14 +23,25 @@ public class Combination {
         }
     }
 
+    /**
+     * nCr = n-1Cr-1 + n-1Cr 공식을 재귀로 표현
+     */
+    public int DFS(int n, int r) {
+        if(mem[n][r] > 0) return mem[n][r];
+        if(n==r || r==0) return 1;
+
+        return mem[n][r] = DFS(n-1, r-1) + DFS(n-1, r);
+    }
+
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
         n = in.nextInt();
         r = in.nextInt();
         mem = new int[n + 1][n + 1];
         Combination combination = new Combination();
-        combination.DFS(0);
-        System.out.println(mem[n][r]);
+//        combination.DFS(0);
+//        System.out.println(mem[n][r]);
+        System.out.println(combination.DFS(n, r));
         return ;
     }
 }
