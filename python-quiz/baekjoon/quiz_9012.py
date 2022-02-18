75540a250e64718375aba943c0d3bacd2ca6eaff
kim-hyunjin <hyunjin1612@gmail.com>
2022-02-18 23:06:33 +0900

    9012 강의 풀이

--- python-quiz/baekjoon/quiz_9012.py
+++ python-quiz/baekjoon/quiz_9012.py
@@ -22,5 +22,29 @@ def main():
     for ps in PSList:
         print(solution(ps))
 
+
+# 강의 풀이
+def solution2():
+    for _ in range(int(input())):
+        stack = []
+        isVPS = True
+        for ch in input():
+            if ch == '(':
+                stack.append(ch)
+            else:
+                if stack:
+                    stack.pop()
+                else:
+                    isVPS = False
+                    break
+
+        if stack:
+            isVPS = False
+
+        print('YES' if isVPS else 'NO')
+
+
+
 if __name__ == "__main__":
     main()
+    # solution2()
