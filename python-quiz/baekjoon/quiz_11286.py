45a3f475969a4131628638d1399458e74e049291
kim-hyunjin <hyunjin1612@gmail.com>
2022-02-18 23:06:21 +0900

    11286 수정

--- python-quiz/baekjoon/quiz_11286.py
+++ python-quiz/baekjoon/quiz_11286.py
@@ -17,7 +17,7 @@ class AbsoluteHeap:
 def main():
     n = int(input())
     hq = AbsoluteHeap()
-    for i in range(n):
+    for _ in range(n):
         x = int(sys.stdin.readline())
         if (x == 0):
             print(hq.pop())
