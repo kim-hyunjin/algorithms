789f2e7fd416c21c503bdb6e8e4fe549dab4f369
kim-hyunjin <hyunjin1612@gmail.com>
2022-02-25 22:24:55 +0900

    1449 강좌 풀이 추가, 정리 노트 추가

--- python-quiz/baekjoon/quiz_1449.py
+++ python-quiz/baekjoon/quiz_1449.py
@@ -22,5 +22,24 @@ def main():
     answer = solution(tapeSize, leakedPositions)
     print(answer)
 
+# 강좌풀이
+def solution2():
+    numOfLeaking, tapeSize = map(int, input().split())
+
+    pipe = [False] * 1001 # pipe의 최대 사이즈는 1000
+    for i in map(int, input().split()):
+        pipe[i] = True # 물이 새는 곳을 True로
+
+    cur = 0
+    answer = 0
+    while cur < 1001:
+        if pipe[cur]:
+            ans += 1
+            cur += tapeSize # tapeSize 만큼 점프
+        else:
+            cur += 1
+    
+    return answer
+
 if __name__ == "__main__":
     main()
